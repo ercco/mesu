a80ef76e1f97f8624b4a821cc6f205c8b0d0c7f6
Tarmo Nurmi <tarmo.nurmi@aalto.fi>
2021-08-20 21:51:55 +0300

    finish algo, not fully tested

--- mesu.py
+++ mesu.py
@@ -25,11 +25,30 @@ def _multilayer_extend_subgraph(M,s,S,VM_extension,t,nl):
             return
     elif not all(len(S[ii])<=s[ii] for ii in range(len(s))):
         return
-    N = {} # TODO
+    N = {neighbor for neighbor in _get_S_neighbors(M,S,t) if t[neighbor] > t[nl]}
     # Exclusive neighborhood to not include nodelayers _within_ the span?
+    while VM_extension:
+        sigma = VM_extension.pop()
+        VM_extension_prime = set(VM_extension)
+        new_S = [S[ii].union({sigma[ii]}) for ii in range(len(sigma))]
+        new_nodelayers = {new_nl for new_nl in itertools.product(*new_S) if new_nl in t}
+        for addition in _additions(S,sigma,t):
+            for neighbor in M[addition]:
+                if neighbor not in new_nodelayers:
+                    if all(t[neighbor_addition] > t[nl] and neighbor_addition not in N for neighbor_addition in _additions(new_S,neighbor,t)):
+                        VM_extension_prime.add(neighbor)
+        _multilayer_extend_subgraph(M,s,new_S,VM_extension_prime,t,nl)
+    return
             
 def _additions(base_S,added_nl,t):
     # return generator with all nodelayers added as result of adding added_nl to base_S
     total_S = [base_S[ii].union({added_nl[ii]}) for ii in range(len(added_nl))]
     init_subgraph_nls = {nl_init for nl_init in itertools.product(*base_S) if nl_init in t}
     return (nl for nl in itertools.product(*total_S) if (nl in t and not nl in init_subgraph_nls))
+
+def _get_S_neighbors(M,S,t):
+    subgraph_nls = {nl for nl in itertools.product(*S) if nl in t}
+    for nl in subgraph_nls:
+        for neighbor in M[nl]:
+            if neighbor not in subgraph_nls:
+                yield neighbor
